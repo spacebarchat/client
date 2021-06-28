af5d64b00bae7ef74a96b12719a30966e64329b5
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-06-28 19:54:05 +0200

    intalize fosscord.js

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Theme from "./components/Theme";
 import Routes from "./components/Routes";
 import Boundary from "./components/Boundary";
+import { Client } from "fosscord.js";
+const client = new Client();
 
 export default function App() {
     return (
