85d48fa554e5171694a11e9f1db32c0f728ce766
stefan080106 <stefan.arnold.programmierung@gmail.com>
2021-08-02 12:55:31 +0200

    fix npm problem

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	presets: ["@babel/preset-react", "module:metro-react-native-babel-preset"],
-	plugins: ["@babel/plugin-syntax-jsx"],
+	plugins: ["@babel/plugin-syntax-jsx", "react-native-reanimated/plugin"],
 };
