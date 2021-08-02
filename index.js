97e47e1ddce2300b79108d5334c4b2b4a0f175d2
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-08-02 19:06:40 +0200

    :construction: drawer

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import "react-native-gesture-handler";
+import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 import { AppRegistry } from "react-native";
 import App from "./src/App";
 import { name as appName } from "./src/app.json";
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -2,42 +2,49 @@ import React from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import DrawerLayout from "react-native-gesture-handler/DrawerLayout";
 import { Box, Text } from "native-base";
+import { Dimensions } from "react-native";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { Icon } from "native-base";
+import KitchenSink from "../pages/DesignEditor/KitchenSink";
 
 // AsyncStorage.removeItem("accessToken");
 
 export default ({ children }: any) => {
+	const width = Dimensions.get("window").width * 0.75;
+
 	return (
 		<DrawerLayout
-			drawerWidth={300}
+			drawerWidth={width}
 			edgeWidth={200}
 			overlayColor="transparent"
 			// @ts-ignore
 			drawerPosition={DrawerLayout.positions.Right}
 			drawerType="slide"
-			drawerBackgroundColor="#ddd"
 			renderNavigationView={() => (
-				<Box>
-					<Text>I am in the drawer!</Text>
+				<Box style={{ borderColor: "white", borderWidth: 1, height: "100%" }}>
+					<Text>right drawer content</Text>
 				</Box>
 			)}
-			onDrawerSlide={(status) => console.log(status)}
 		>
 			<DrawerLayout
 				overlayColor="transparent"
 				edgeWidth={200}
-				drawerWidth={300}
+				drawerWidth={width}
 				// @ts-ignore
 				drawerPosition={DrawerLayout.positions.Left}
 				drawerType="slide"
-				drawerBackgroundColor="#ddd"
 				renderNavigationView={() => (
-					<Box>
-						<Text>I am in the drawer!</Text>
+					<Box style={{ borderColor: "white", borderWidth: 1, height: "100%" }}>
+						<Text>left drawer content</Text>
 					</Box>
 				)}
-				onDrawerSlide={(status) => console.log(status)}
 			>
-				<Text>Hello, it's second</Text>
+				<Box style={{ borderColor: "white", borderWidth: 1, height: "100%" }}>
+					<Icon as={FontAwesome} name="home" />
+					<Icon as={Ionicons} name="home" />
+					<KitchenSink />
+					<Text>content</Text>
+				</Box>
 			</DrawerLayout>
 		</DrawerLayout>
 	);
