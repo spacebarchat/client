51d498d86c129c46471ee671ec6100cc1df36c11
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-08-06 00:40:18 +0200

    Revert ":robot: add splashcreen for android"
    This reverts commit ad57491a6b86a4c1e48c356a7e260704b0ebd27b.

--- android/app/src/main/java/com/fosscord/MainApplication.java
+++ android/app/src/main/java/com/fosscord/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactInstanceManager;
