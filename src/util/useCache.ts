4db9dda70cb20a97519ef2be0c9f6c80cc0d21a3
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-08-05 14:28:41 +0200

    :bug: fix useCache

--- src/util/useCache.ts
+++ src/util/useCache.ts
@@ -1,5 +1,5 @@
 import { Collection } from "fosscord.js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function useCache<T extends Collection<any, any>>(o: T | { cache: T }): T {
 	const [state, setState] = useState(0);
@@ -7,9 +7,17 @@ export function useCache<T extends Collection<any, any>>(o: T | { cache: T }): T
 	// @ts-ignore
 	const collection: Collection<any, any> = o.cache || o;
 
-	collection.events.on("changed", (key: any) => {
+	const onChanged = (key: any) => {
 		setState(state + 1);
-	});
+	};
+
+	useEffect(() => {
+		collection.events.on("changed", onChanged);
+
+		return () => {
+			collection.events.off("changed", onChanged);
+		};
+	}, [o]);
 
 	// @ts-ignore
 	return collection;
