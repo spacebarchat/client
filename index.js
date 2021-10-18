911b2361b8632766bb7c1b01274ec94e5fced1a6
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-10-18 21:43:23 +0200

    :sparkles: linux support with valence-native (qt, wx)

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-/**
- * @format
- */
-
-// import { setJSExceptionHandler, getJSExceptionHandler } from "react-native-exception-handler";
-// import { setNativeExceptionHandler } from "react-native-exception-handler";
-
-// const exceptionhandler = (error, isFatal) => {
-// 	console.error(error, isFatal);
-// };
-// setNativeExceptionHandler(exceptionhandler, true);
-// setJSExceptionHandler(exceptionhandler, true);
-
 import { AppRegistry } from "react-native";
 import App from "./src/App";
 import { name as appName } from "./src/app.json";
 import { NativeModules } from "react-native";
-NativeModules.DevSettings.setIsSecondaryClickToShowDevMenuEnabled?.(false);
+NativeModules?.DevSettings.setIsSecondaryClickToShowDevMenuEnabled?.(false);
 
 AppRegistry.registerComponent(appName, () => App);
+
+if (module.hot) {
+	module.hot.accept(["./src/app"], function () {
+		const app = require("./src/App")["default"];
+		AppRegistry.updateProxy(app);
+	});
+}
