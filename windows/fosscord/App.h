f1fe74916b0c66902379a307e6a6efbfe8633bc6
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-10-24 14:40:35 +0200

    :sparkles: app center with auto update

--- windows/fosscord/App.h
+++ windows/fosscord/App.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "App.xaml.g.h"
-
+#include "winrt/Microsoft.ReactNative.h"
 #include <CppWinRTIncludes.h>
 
 #ifdef USE_WINUI3
@@ -15,11 +15,12 @@ namespace winrt::fosscord::implementation
     struct App : AppT<App>
     {
         App() noexcept;
-        void OnLaunched(activation::LaunchActivatedEventArgs const&);
-        void OnActivated(Windows::ApplicationModel::Activation::IActivatedEventArgs const &e);
+        void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs const&);
         void OnSuspending(IInspectable const&, Windows::ApplicationModel::SuspendingEventArgs const&);
-        void OnNavigationFailed(IInspectable const&, xaml::Navigation::NavigationFailedEventArgs const&);
-      private:
-        using super = AppT<App>;
+        void OnNavigationFailed(IInspectable const&, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs const&);
+    
+        winrt::Microsoft::ReactNative::ReactNativeHost& Host() noexcept { return m_host; }
+    private:
+        winrt::Microsoft::ReactNative::ReactNativeHost m_host;
     };
 } // namespace winrt::fosscord::implementation
--- windows/fosscord/AutolinkedNativeModules.g.cpp
+++ windows/fosscord/AutolinkedNativeModules.g.cpp
@@ -3,12 +3,16 @@
 #include "pch.h"
 #include "AutolinkedNativeModules.g.h"
 
+// Includes from react-native-code-push
+#include <winrt/Microsoft.CodePush.ReactNative.h>
+
 namespace winrt::Microsoft::ReactNative
 {
 
 void RegisterAutolinkedNativeModulePackages(winrt::Windows::Foundation::Collections::IVector<winrt::Microsoft::ReactNative::IReactPackageProvider> const& packageProviders)
 { 
-    UNREFERENCED_PARAMETER(packageProviders);
+    // IReactPackageProviders from react-native-code-push
+    packageProviders.Append(winrt::Microsoft::CodePush::ReactNative::ReactPackageProvider());
 }
 
 }
