fa57cb3a07646ecbc41ec4a5f20b728a04b2340b
Intevel ツ <59223342+Intevel@users.noreply.github.com>
2021-10-25 16:16:09 +0200

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,10 @@
 import React, { useRef } from "react";
-import { Box, Button, FormControl, Input, Text, View, VStack } from "native-base";
+import { Box, Button, FormControl, Input, Text, View, VStack, Heading, Image } from "native-base";
 import { Keyboard, StyleSheet, TouchableWithoutFeedback } from "react-native";
 import Styles, { relativeScreenHeight, useMobile } from "../util/Styles";
 import KeyboardAvoidingView from "../components/KeyboardAvoidingView";
 import { useHistory } from "react-router";
+import { tailwind } from "../util/tailwind";
 
 const styles = StyleSheet.create({
 	formInputSection: {
@@ -26,64 +27,42 @@ export default function Login() {
 
 	return (
 		<>
-			<KeyboardAvoidingView behavior="padding" style={Styles.h100}>
-				<Box safeArea style={[Styles.h100, { alignItems: "center", display: "flex" }]}>
-					<VStack
-						rounded={5}
-						style={{ width: "100%", maxWidth: 480, height: isMobile ? "100%" : undefined }}
-						top={isMobile ? 0 : relativeScreenHeight(10)}
-						justifyContent="space-between"
-						display="flex"
-						borderWidth={isMobile ? 0 : "1"}
-						borderColor="text"
-						p="5"
-					>
-						<View>
-							<TouchableWithoutFeedback onPress={openModal}>
-								<FormControl
-									// @ts-ignore
-									style={[styles.formInputSection, { outline: "none" }]}
-								>
-									<FormControl.Label>Instance</FormControl.Label>
-									<Box
-										width="100%"
-										height={10}
-										borderWidth="1"
-										borderColor="text"
-										padding={3}
-										borderRadius="5"
-										display="flex"
-										justifyContent="center"
-									>
-										<Text fontSize={15} color="#a3a3a3">
-											Select an instance
-										</Text>
-									</Box>
-								</FormControl>
-							</TouchableWithoutFeedback>
-							<FormControl style={styles.formInputSection}>
-								<FormControl.Label>Email</FormControl.Label>
-								<Input
-									blurOnSubmit={false}
-									onSubmitEditing={() => {
-										passwordElement?.current?.focus();
-									}}
-									returnKeyType="next"
-									ref={emailElement}
-									type="email"
-									placeholder="Enter email"
-								/>
-							</FormControl>
-							<FormControl style={styles.formInputSection}>
-								<FormControl.Label>Password</FormControl.Label>
-								<Input ref={passwordElement} type="password" placeholder="Enter password" />
-							</FormControl>
-						</View>
-						<Button onPress={() => history.push("/")} style={{ margin: 10 }}>
-							Login
-						</Button>
-					</VStack>
-				</Box>
+			<KeyboardAvoidingView behavior='padding' style={tailwind("h-full px-8 py-14")}>
+				<Image
+					source={{
+						uri: "https://github.com/fosscord/fosscord-client/blob/master/ios/fosscord/logo.png?raw=true",
+					}}
+					alt='Alternate Text'
+					style={tailwind("mt-20 w-8 h-8")}
+				/>
+				<Heading style={tailwind("text-white font-extrabold mt-12 text-4xl")}>Hey,{"\n"}Login now!</Heading>
+				<Heading style={tailwind("text-gray-200 font-light mt-4 text-xl")}>Explore Instances</Heading>
+				<FormControl style={tailwind("mt-14")}>
+					<FormControl.Label>
+						<Text style={tailwind("text-white")}>Email</Text>
+					</FormControl.Label>
+					<Input
+						blurOnSubmit={false}
+						onSubmitEditing={() => {
+							passwordElement?.current?.focus();
+						}}
+						returnKeyType='next'
+						ref={emailElement}
+						type='email'
+						placeholder='Email'
+						style={tailwind("bg-gray-200 h-14 rounded-lg")}
+					/>
+				</FormControl>
+				<FormControl style={tailwind("mt-6")}>
+					<FormControl.Label>
+						<Text style={tailwind("text-white")}>Password</Text>
+					</FormControl.Label>
+					<Input ref={passwordElement} type='password' placeholder='Password' style={tailwind("bg-gray-200 h-14 rounded-lg")} />
+				</FormControl>
+				<Heading style={tailwind("text-gray-200 font-light mt-4 text-sm opacity-70")}>Forgot your password? </Heading>
+				<Button style={tailwind("mt-28 h-16 bg-primary rounded-xl")} onPress={() => history.push("/")}>
+					<Text style={tailwind("font-semibold text-lg text-white")}>Login to fosscord.com</Text>
+				</Button>
 			</KeyboardAvoidingView>
 		</>
 	);
