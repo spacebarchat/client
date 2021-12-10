f6544b693bc0f33c8888cb214971c3df79bf16da
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-12-10 21:47:51 +0100

    :bug: fix ios theme

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	presets: ["@babel/preset-react", "module:metro-react-native-babel-preset"],
 	plugins: [
 		["@babel/plugin-proposal-decorators", { legacy: true }],
-		["@babel/plugin-proposal-class-properties", { loose: false }],
+		// ["@babel/plugin-proposal-class-properties", { loose: false }],
 	],
 	assumptions: {
 		setPublicClassFields: false,
