c3cb3a09abbb41d915c237c8bebf21d999803d88
Casraw <casraw70@googlemail.com>
2023-03-10 20:10:34 +0100

    typo fixing

--- src/constants/Globals.ts
+++ src/constants/Globals.ts
@@ -13,13 +13,13 @@ interface RouteSettings {
 }
 
 export const DefaultRouteSettings: RouteSettings = {
-  api: 'https://${SEVER_URL}/api',
-  cdn: 'https://${SEVER_URL}',
-  invite: 'https://${SEVER_URL}/invite',
-  template: 'https://${SEVER_URL}/template',
-  gift: 'https://${SEVER_URL}/gift',
-  scheduledEvent: 'https://${SEVER_URL}/events',
-  gateway: 'wss://${SEVER_URL}',
+  api: 'https://${SERVER_URL}/api',
+  cdn: 'https://${SERVER_URL}',
+  invite: 'https://${SERVER_URL}/invite',
+  template: 'https://${SERVER_URL}/template',
+  gift: 'https://${SERVER_URL}/gift',
+  scheduledEvent: 'https://${SERVER_URL}/events',
+  gateway: 'wss://${SERVER_URL}',
 };
 
 export const Globals: {
