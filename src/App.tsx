26d996c695cd9658aa009db9d43c4f7b833c714f
Puyodead1 <puyodead@proton.me>
2023-03-20 17:56:00 -0400

    aaaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
+import Typography from "@mui/material/Typography";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Typography variant="h1" component="h2">
+        Fosscord Client
+      </Typography>
+      ;
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
