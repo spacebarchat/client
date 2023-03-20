643dbb7e1d2c12dc3ac46a704c3280ed8b75b050
Puyodead1 <puyodead@proton.me>
2023-03-20 17:25:10 -0400

    mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
