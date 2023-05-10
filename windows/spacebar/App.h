e14f73e8e477bbe4b69810468d5cbc364ed39eeb
Puyodead1 <puyodead@protonmail.com>
2023-05-10 07:53:19 -0400

    more renaming

---
+++ windows/spacebar/App.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+#include <CppWinRTIncludes.h>
+
+namespace activation = winrt::Windows::ApplicationModel::Activation;
+
+namespace winrt::spacebar::implementation
+{
+  struct App : AppT<App>
+  {
+    App() noexcept;
+    void OnLaunched(activation::LaunchActivatedEventArgs const &);
+    void OnActivated(Windows::ApplicationModel::Activation::IActivatedEventArgs const &e);
+    void OnSuspending(IInspectable const &, Windows::ApplicationModel::SuspendingEventArgs const &);
+    void OnNavigationFailed(IInspectable const &, xaml::Navigation::NavigationFailedEventArgs const &);
+
+  private:
+    using super = AppT<App>;
+  };
+} // namespace winrt::spacebar::implementation
---
+++ windows/spacebar/MainPage.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::spacebar::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::spacebar::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
