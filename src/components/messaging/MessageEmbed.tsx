647373209f50b02d223b6f7f9c6c084258c12e1f
Puyodead1 <puyodead@proton.me>
2023-09-03 22:35:22 -0400

    rewrite embeds a bit to work correctly

--- src/components/messaging/MessageEmbed.tsx
+++ src/components/messaging/MessageEmbed.tsx
@@ -1,4 +1,5 @@
 import { APIAttachment, APIEmbed, EmbedType } from "@spacebarchat/spacebar-api-types/v9";
+import { ReactNode } from "react";
 import styled from "styled-components";
 import useLogger from "../../hooks/useLogger";
 import { decimalColorToHex } from "../../utils/Utils";
@@ -14,51 +15,101 @@ interface EmbedProps {
 	contextMenuItems: IContextMenuItem[];
 }
 
-const EmbedContainer = styled.div<{ $type: EmbedType; $color?: string }>`
-	padding: 10px;
-	margin-top: 5px;
+const Container = styled.div<{ $color?: string }>`
+	max-width: 430px;
+	justify-self: start;
+	border-left-width: 4px;
+	border-left-style: solid;
+	border-left-color: ${(props) => props.$color ?? "var(--background-tertiary)"};
 	background: var(--background-secondary);
+	display: grid;
+	box-sizing: border-box;
 	border-radius: 4px;
+`;
+
+const Wrapper = styled.div`
+	max-width: 500px;
+	overflow: hidden;
+	padding: 8px 16px 16px 12px;
 	display: grid;
 	grid-template-columns: auto;
 	grid-template-rows: auto;
-	max-width: 500px;
-	width: ${(props) => (props.$type == EmbedType.Link ? undefined : "min-content")};
-
-	border-left-width: 5px;
-	border-left-color: ${(props) => props.$color ?? "var(--background-tertiary)"};
-	border-left-style: solid;
 `;
 
 const EmbedProvider = styled.div`
 	font-size: 12px;
 	font-weight: var(--font-weight-regular);
+	grid-column: 1/1;
 	margin-top: 10px;
 `;
 
-const EmbedHeader = styled.div`
-	color: var(--primary-light);
+const EmbedAuthor = styled.div`
+	display: flex;
+	align-items: center;
+	grid-column: 1/1;
+	margin-top: 10px;
+`;
+
+const EmbedAuthorText = styled.div`
+	font-size: 14px;
+	font-weight: var(--font-weight-medium);
+`;
+
+const EmbedAuthorLink = styled.a`
+	font-size: 14px;
+	font-weight: var(--font-weight-medium);
+	text-decoration: none;
+	&:hover {
+		text-decoration: underline;
+	}
+`;
+
+const EmbedTitle = styled.div`
+	font-size: 16px;
 	font-weight: var(--font-weight-regular);
 	grid-column: 1/1;
 	margin-top: 10px;
 `;
 
+const EmbedTitleLink = styled.a`
+	color: var(--text-link);
+	text-decoration: none;
+	&:hover {
+		text-decoration: underline;
+	}
+`;
+
 const EmbedDescription = styled.div`
-	font-weight: var(--font-weight-light);
 	font-size: 14px;
+	font-weight: var(--font-weight-regular);
 	grid-column: 1/1;
 	margin-top: 10px;
 `;
 
-const EmbedImageArticle = styled.div`
-	margin-top: 16px;
+const EmbedImage = styled.div`
+	margin-top: 10px;
 	grid-column: 1/1;
+	border-radius: 4px;
 `;
 
-const EmbedImageLink = styled.div`
+const EmbedImageContainer = styled.div`
+	flex-flow: row nowrap;
+	width: 100%;
+	height: 100%;
+	display: flex;
+`;
+
+const EmbedImageWrapper = styled.div`
+	max-width: 100%;
+	width: 100%;
+	overflow: hidden;
+	border-radius: 4px;
+`;
+
+const EmbedThumbnail = styled.div`
 	grid-row: 1/8;
 	grid-column: 2/2;
-	margin-left: 16px;
+	margin-left: 15px;
 	margin-top: 10px;
 	justify-self: end;
 `;
@@ -71,22 +122,38 @@ const EmbedFooter = styled.div`
 	margin-top: 10px;
 `;
 
+const EmbedFooterImage = styled.img`
+	margin-right: 10px;
+	width: 20px;
+	height: 20px;
+	border-radius: 50%;
+`;
+
 const EmbedFooterText = styled.span`
 	font-size: 12px;
 	font-weight: var(--font-weight-regular);
 `;
 
 const YoutubeEmbed = styled.iframe`
-	display: block;
 	outline: none;
 	border: none;
 	margin-top: 10px;
+	border-radius: 4px;
 `;
 
-const createEmbedAttachment = (embed: APIEmbed, contextMenuItems: IContextMenuItem[]) => {
-	const url = new URL(embed.url!);
+const WrapImageContent = ({ children }: { children: ReactNode }) => {
+	return (
+		<EmbedImageContainer>
+			<EmbedImageWrapper>{children}</EmbedImageWrapper>
+		</EmbedImageContainer>
+	);
+};
+
+const createEmbedAttachment = (embed: APIEmbed, contextMenuItems: IContextMenuItem[], isYoutubeVideo = false) => {
 	const image = embed.thumbnail ?? embed.image;
 	if (!image) return null;
+
+	const url = new URL(embed.url!);
 
 	const fakeAttachment: APIAttachment = {
 		id: embed.url as string,
@@ -104,71 +171,87 @@ const createEmbedAttachment = (embed: APIEmbed, contextMenuItems: IContextMenuIt
 		attachment: fakeAttachment,
 	};
 
-	if (embed.type == EmbedType.Link) {
+	if (isYoutubeVideo) return createYoutubeEmbed(embed);
+
+	if (embed.type === EmbedType.Link)
 		return (
-			<EmbedImageLink>
-				<MessageAttachment {...props} maxWidth={160} maxHeight={80} />
-			</EmbedImageLink>
+			<EmbedThumbnail>
+				<WrapImageContent>
+					<MessageAttachment {...props} maxWidth={70} />
+				</WrapImageContent>
+			</EmbedThumbnail>
 		);
-	} else if (embed.type == EmbedType.Article || embed.type == EmbedType.Rich) {
-		return (
-			<EmbedImageArticle>
+	return (
+		<EmbedImage>
+			<WrapImageContent>
 				<MessageAttachment {...props} />
-			</EmbedImageArticle>
-		);
-	} else
-		return (
-			<div
-				style={{
-					marginTop: "10px",
-				}}
-			>
-				<MessageAttachment {...props} />
-			</div>
-		);
+			</WrapImageContent>
+		</EmbedImage>
+	);
 };
 
 const createYoutubeEmbed = (embed: APIEmbed) => {
-	return <YoutubeEmbed width={500} height={300} src={embed.video!.url} />;
+	return <YoutubeEmbed width={400} height={225} src={embed.video!.url} />;
 };
 
 export default function MessageEmbed({ embed, contextMenuItems }: EmbedProps) {
 	const logger = useLogger("MessageEmbed");
 
 	// seems like the server sometimes sends thumbnails with 0 width and height, and no urls
-	const image = embed.thumbnail ?? embed.image;
 	const isYoutubeVideo = embed.type == EmbedType.Video && embed.provider?.name == "YouTube";
-	const thumbnail = image && image.url && !isYoutubeVideo ? createEmbedAttachment(embed, contextMenuItems) : null;
+	const thumbnail = createEmbedAttachment(embed, contextMenuItems, isYoutubeVideo);
 
 	if (embed.type == EmbedType.Image) return thumbnail;
 
+	const titleTrimmed = embed.title
+		? embed.title?.length > TITLE_MAX_CHARS
+			? embed.title.substring(0, TITLE_MAX_CHARS) + "..."
+			: embed.title
+		: undefined;
+
+	const descriptionTrimmed = embed.description
+		? embed.description.length > DESCRIPTION_MAX_CHARS
+			? embed.description?.substring(0, DESCRIPTION_MAX_CHARS) + "..."
+			: embed.description
+		: undefined;
+
+	let title;
+	if (titleTrimmed) {
+		if (embed.url)
+			title = (
+				<EmbedTitleLink href={embed.url} rel="noreferrer noopener" target="_blank">
+					{titleTrimmed}
+				</EmbedTitleLink>
+			);
+		else title = titleTrimmed;
+	} else title = null;
+
+	let author;
+	if (embed.author)
+		if (embed.author.url)
+			author = (
+				<EmbedAuthorLink href={embed.author.url} rel="noreferrer noopener" target="_blank">
+					{embed.author.name}
+				</EmbedAuthorLink>
+			);
+		else author = <EmbedAuthorText>{embed.author.name}</EmbedAuthorText>;
+	else null;
+
 	return (
-		<EmbedContainer
-			$type={embed.type ?? EmbedType.Link}
-			$color={embed.color ? decimalColorToHex(embed.color) : undefined}
-		>
-			{embed.provider && <EmbedProvider>{embed.provider.name}</EmbedProvider>}
-			{embed.title && (
-				<EmbedHeader>
-					{embed.title.length > TITLE_MAX_CHARS
-						? embed.title.substring(0, TITLE_MAX_CHARS) + "..."
-						: embed.title}
-				</EmbedHeader>
-			)}
-			{embed.description && !isYoutubeVideo && (
-				<EmbedDescription>
-					{embed.description.length > DESCRIPTION_MAX_CHARS
-						? embed.description.substring(0, DESCRIPTION_MAX_CHARS) + "..."
-						: embed.description}
-				</EmbedDescription>
-			)}
-			{thumbnail}
-			{isYoutubeVideo && createYoutubeEmbed(embed)}
-			{embed.footer && (
-				<EmbedFooter>
-					<EmbedFooterText>{embed.footer.text}</EmbedFooterText>
-				</EmbedFooter>
-			)}
-		</EmbedContainer>
+		<Container $color={embed.color ? decimalColorToHex(embed.color) : undefined}>
+			<Wrapper>
+				{embed.provider && <EmbedProvider>{embed.provider.name}</EmbedProvider>}
+				{author && <EmbedAuthor>{author}</EmbedAuthor>}
+				{title && <EmbedTitle>{title}</EmbedTitle>}
+				{descriptionTrimmed && !isYoutubeVideo && <EmbedDescription>{descriptionTrimmed}</EmbedDescription>}
+				{thumbnail}
+				{embed.footer && (
+					<EmbedFooter>
+						{embed.footer.icon_url && <EmbedFooterImage src={embed.footer.icon_url} />}
+						<EmbedFooterText>{embed.footer.text}</EmbedFooterText>
+					</EmbedFooter>
+				)}
+			</Wrapper>
+		</Container>
 	);
 }
