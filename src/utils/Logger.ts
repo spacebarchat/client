2f30c9bb1b37af2f908dc57a10cc39434e20dd7d
Puyodead1 <puyodead@protonmail.com>
2023-09-04 22:39:58 -0400

    fix all log levels being debug

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -10,14 +10,14 @@ export default class Logger {
 	}
 
 	info(...args: any[]) {
-		console.debug(`%c${new Date().toLocaleTimeString()} | ${this.name} | INFO |`, `color: DodgerBlue`, ...args);
+		console.info(`%c${new Date().toLocaleTimeString()} | ${this.name} | INFO |`, `color: DodgerBlue`, ...args);
 	}
 
 	warn(...args: any[]) {
-		console.debug(`%c${new Date().toLocaleTimeString()} | ${this.name} | WARN |`, `color: Tomato`, ...args);
+		console.warn(`%c${new Date().toLocaleTimeString()} | ${this.name} | WARN |`, `color: Tomato`, ...args);
 	}
 
 	error(...args: any[]) {
-		console.debug(`%c${new Date().toLocaleTimeString()} | ${this.name} | ERROR |`, `color: Red`, ...args);
+		console.error(`%c${new Date().toLocaleTimeString()} | ${this.name} | ERROR |`, `color: Red`, ...args);
 	}
 }
