6c809d8779db0d8923b00ff553d314a231461efb
Puyodead1 <puyodead@proton.me>
2023-09-04 21:57:00 -0400

    add some plugins

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,13 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use tauri::Manager;
+use tauri_plugin_autostart::MacosLauncher;
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+    args: Vec<String>,
+    cwd: String,
+}
 
 #[tauri::command]
 async fn close_splashscreen(window: tauri::Window) {
@@ -16,6 +23,32 @@ async fn close_splashscreen(window: tauri::Window) {
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![close_splashscreen])
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_autostart::init(
+            MacosLauncher::LaunchAgent,
+            Some(vec![]),
+        ))
+        .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            println!("{}, {argv:?}, {cwd}", app.package_info().name);
+
+            app.emit_all("single-instance", Payload { args: argv, cwd })
+                .unwrap();
+        }))
+        .setup(|app| {
+            #[cfg(desktop)]
+            app.handle().plugin(tauri_plugin_authenticator::init())?;
+            Ok(())
+        })
+        // .setup(|app| {
+        //     #[cfg(desktop)]
+        //     app.handle()
+        //         .plugin(tauri_plugin_updater::Builder::new().build())?;
+        //     Ok(())
+        // })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
