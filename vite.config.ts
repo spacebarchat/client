0f14eda999cbf1e395ac5a0aa656332f23454f73
Puyodead1 <puyodead@proton.me>
2023-09-07 11:25:35 -0400

    android

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
 import replace from "@rollup/plugin-replace";
 import react from "@vitejs/plugin-react";
 import { readFileSync } from "fs";
+import { internalIpV4 } from "internal-ip";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
@@ -40,7 +41,10 @@ function getVersion() {
 	return JSON.parse(readFileSync("package.json").toString()).version;
 }
 
-export default defineConfig({
+const mobile = !!/android|ios/.exec(process.env.TAURI_PLATFORM);
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
 	plugins: [
 		react(),
 		svgr(),
@@ -52,11 +56,32 @@ export default defineConfig({
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		}) as any,
 	],
+
+	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+	//
+	// 1. prevent vite from obscuring rust errors
+	clearScreen: false,
+	// 2. tauri expects a fixed port, fail if that port is not available
+	server: {
+		host: mobile ? "0.0.0.0" : false,
+		port: 1420,
+		hmr: mobile
+			? {
+					protocol: "ws",
+					host: await internalIpV4(),
+					port: 1421,
+			  }
+			: undefined,
+		strictPort: true,
+	},
 	optimizeDeps: {
 		esbuildOptions: {
 			plugins: [esbuildCommonjs(["react-moment"])],
 		},
 	},
+	// 3. to make use of `TAURI_DEBUG` and other env variables
+	// https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+	envPrefix: ["VITE_", "TAURI_"],
 	build: {
 		sourcemap: true,
 		rollupOptions: {
@@ -66,15 +91,7 @@ export default defineConfig({
 			output: {
 				dir: "build",
 				chunkFileNames: "[hash:20].js",
-				entryFileNames: "[hash:20].js",
-				inlineDynamicImports: false,
-				experimentalMinChunkSize: 500_000,
-				manualChunks(id) {
-					if (id.includes("node_modules")) {
-						return id.toString().split("node_modules/")[1].split("/")[0].toString();
-					}
-				},
 			},
 		},
 	},
-});
+}));
