03da3990a1b42a2724ef1f7a2b445da1898b510c
Puyodead1 <puyodead@proton.me>
2023-09-07 11:25:35 -0400

    android

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,53 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
-use tauri_plugin_autostart::MacosLauncher;
-
-#[derive(Clone, serde::Serialize)]
-struct Payload {
-    args: Vec<String>,
-    cwd: String,
-}
-
-#[tauri::command]
-async fn close_splashscreen(window: tauri::Window) {
-    // Close splashscreen
-    if let Some(splashscreen) = window.get_window("splashscreen") {
-        splashscreen.close().unwrap();
-    }
-    // Show main window
-    window.get_window("main").unwrap().show().unwrap();
-}
-
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![close_splashscreen])
-        .plugin(tauri_plugin_clipboard_manager::init())
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_autostart::init(
-            MacosLauncher::LaunchAgent,
-            Some(vec![]),
-        ))
-        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
-            println!("{}, {argv:?}, {cwd}", app.package_info().name);
-
-            app.emit_all("single-instance", Payload { args: argv, cwd })
-                .unwrap();
-        }))
-        .setup(|app| {
-            #[cfg(desktop)]
-            app.handle().plugin(tauri_plugin_authenticator::init())?;
-            Ok(())
-        })
-        // .setup(|app| {
-        //     #[cfg(desktop)]
-        //     app.handle()
-        //         .plugin(tauri_plugin_updater::Builder::new().build())?;
-        //     Ok(())
-        // })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    #[cfg(desktop)]
+    tauri_app_lib::run();
 }
