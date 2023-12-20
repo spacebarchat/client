3b00be10298e158f757b6cf24e09ba08f4217434
Puyodead1 <puyodead@proton.me>
2023-12-20 09:49:25 -0500

    tauri: tray icon

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,6 @@
 #[cfg(desktop)]
 use tauri::Manager;
+mod tray;
 
 #[tauri::command]
 async fn close_splashscreen(window: tauri::Window) {
@@ -21,6 +22,22 @@ pub fn run() {
     std::env::set_var("RUST_LOG", "debug");
 
     tauri::Builder::default()
+        .setup(move |app| {
+            #[cfg(desktop)]
+            {
+                let handle = app.handle();
+                tray::create_tray(handle)?;
+            }
+
+            Ok(())
+        })
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::CloseRequested { api, .. } => {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+            _ => {}
+        })
         .invoke_handler(tauri::generate_handler![close_splashscreen,])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
