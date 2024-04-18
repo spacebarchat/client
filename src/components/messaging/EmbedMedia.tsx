24c177517e4f50054d4b166aa8d10bb9225ee78d
Puyodead1 <puyodead@proton.me>
2024-04-17 23:01:46 -0400

    more fixes for embeds

--- src/components/messaging/EmbedMedia.tsx
+++ src/components/messaging/EmbedMedia.tsx
@@ -6,6 +6,27 @@ import { modalController } from "../../controllers/modals";
 import Icon from "../Icon";
 import styles from "./Embed.module.css";
 
+function getScaledDimensions(originalWidth: number, originalHeight: number, maxWidth: number, maxHeight: number) {
+	const aspectRatio = originalWidth / originalHeight;
+	let newWidth = originalWidth;
+	let newHeight = originalHeight;
+
+	if (newWidth > maxWidth) {
+		newWidth = maxWidth;
+		newHeight = newWidth / aspectRatio;
+	}
+
+	if (newHeight > maxHeight) {
+		newHeight = maxHeight;
+		newWidth = newHeight * aspectRatio;
+	}
+
+	return { width: Math.round(newWidth), height: Math.round(newHeight) };
+}
+function shouldScaleImage(originalWidth: number, originalHeight: number, maxWidth: number, maxHeight: number) {
+	return originalWidth > maxWidth || originalHeight > maxHeight;
+}
+
 interface Props {
 	embed: APIEmbed;
 	width?: number;
@@ -14,12 +35,49 @@ interface Props {
 }
 
 function EmbedMedia({ embed, width, height, thumbnail }: Props) {
+	let maxWidth = 400;
+	let maxHeight = 300;
+
+	if (!width || !height) {
+		if (embed.video) {
+			width = embed.video.width;
+			height = embed.video.height;
+		} else if (embed.image) {
+			width = embed.image.width;
+			height = embed.image.height;
+		} else if (embed.thumbnail) {
+			if (embed.type !== EmbedType.Image && embed.provider?.name !== "GitHub") {
+				maxWidth = 80;
+				maxHeight = 80;
+			}
+
+			width = embed.thumbnail.width;
+			height = embed.thumbnail.height;
+		} else {
+			console.log("No media size provided");
+			width = 400;
+			height = 300;
+		}
+	}
+
+	const originalWidth = width;
+	const originalHeight = height;
+
+	// Scale image if it's too large
+	if (shouldScaleImage(width!, height!, maxWidth, maxHeight)) {
+		const { width: newWidth, height: newHeight } = getScaledDimensions(width!, height!, maxWidth, maxHeight);
+		width = newWidth;
+		height = newHeight;
+	}
+
+	console.log(`Original size: ${originalWidth}x${originalHeight} - Scaled size: ${width}x${height}`);
+
 	switch (embed.provider?.name) {
 		case "YouTube": {
 			if (!embed.video?.url) return null;
 			const url = embed.video.url;
 
-			return <iframe loading="lazy" src={url} allowFullScreen style={{ height }} />;
+			return <iframe loading="lazy" src={url} allowFullScreen style={{ height, width }} />;
 		}
 		case "Spotify": {
 			const url = embed.url;
@@ -144,7 +202,7 @@ function EmbedMedia({ embed, width, height, thumbnail }: Props) {
 						className={thumbnail ? styles.embedThumbnail : styles.embedImage}
 						src={url}
 						loading="lazy"
-						style={{ width: thumbnail ? width : undefined, height }}
+						style={{ height, width }}
 						onClick={() => {
 							modalController.push({
 								type: "image_viewer",
