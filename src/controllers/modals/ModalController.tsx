05e70df75cbfd2ab557b2f991bb4bbc27b431f0c
Puyodead1 <puyodead@proton.me>
2024-10-16 14:08:41 -0400

    fix an error

--- src/controllers/modals/ModalController.tsx
+++ src/controllers/modals/ModalController.tsx
@@ -102,10 +102,10 @@ class ModalController<T extends Modal> {
 	get rendered() {
 		return (
 			<>
-				{this.stack.map((modal) => {
-					const Component = this.components[modal.type];
+				{this.stack.map(({ key, ...rest }) => {
+					const Component = this.components[rest.type];
 					if (!Component) return null;
-					return <Component {...modal} onClose={() => this.remove(modal.key!)} />;
+					return <Component key={key} {...rest} onClose={() => this.remove(key!)} />;
 				})}
 			</>
 		);
