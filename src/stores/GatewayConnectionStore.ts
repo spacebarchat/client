19e496a712182387c36b2b4b6788e50bbde08e33
MathMan05 <73901602+MathMan05@users.noreply.github.com>
2024-12-18 11:20:51 -0600

    Maybe fix DDOS

--- src/stores/GatewayConnectionStore.ts
+++ src/stores/GatewayConnectionStore.ts
@@ -114,9 +114,12 @@ export default class GatewayConnectionStore {
 		this.logger.debug(`[Disconnect] ${this.url}`);
 		this.socket?.close(code, reason);
 	}
-
+	reconnecting=false;
 	startReconnect() {
+		if(reconnecting)return;
+		reconnecting=true;
 		setTimeout(() => {
+			reconnecting=false;
 			this.logger.debug("Starting reconnect...");
 			this.connect(this.url!);
 		}, this.reconnectTimeout);
